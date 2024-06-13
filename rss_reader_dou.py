2bfc148bc9673ce1f26420f5169d0400809ef989
Petro Zamkovyi <petro.zamkovyi@dxc.com>
2024-06-13 22:20:46 +0300

    adding additional links to scrap cumulative data

--- rss_reader_dou.py
+++ rss_reader_dou.py
@@ -2,12 +2,21 @@ import requests
 import xml.etree.ElementTree as ET
 import json
 from typing import List, Dict
+import os
+from datetime import datetime
 
 # Constants
-RSS_FEED_URL = 'https://jobs.dou.ua/vacancies/feeds/?category=Golang'
+RSS_FEED_URLS = [
+    'https://jobs.dou.ua/vacancies/feeds/?exp=0-1&category=Golang',
+    'https://jobs.dou.ua/vacancies/feeds/?exp=1-3&category=Golang',
+    'https://jobs.dou.ua/vacancies/feeds/?exp=3-5&category=Golang',
+    'https://jobs.dou.ua/vacancies/feeds/?exp=5plus&category=Golang'
+]
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
+OUTPUT_DIR = 'extracted_data'
+CUMULATIVE_FILE = os.path.join(OUTPUT_DIR, 'cumulative.json')
 
 
 def fetch_rss_feed(url: str, headers: Dict[str, str]) -> ET.Element:
@@ -37,16 +46,39 @@ def extract_rss_items(root: ET.Element) -> List[Dict[str, str]]:
     return rss_items
 
 
+def save_to_json(data: List[Dict[str, str]], filename: str):
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
+
+
+def update_cumulative_file(new_data: List[Dict[str, str]], cumulative_file: str):
+    if os.path.exists(cumulative_file):
+        with open(cumulative_file, 'r', encoding='utf-8') as file:
+            cumulative_data = json.load(file)
+    else:
+        cumulative_data = []
+
+    cumulative_data.extend(new_data)
+    save_to_json(cumulative_data, cumulative_file)
+
+
 def main():
-    try:
-        root = fetch_rss_feed(RSS_FEED_URL, HEADERS)
-        rss_items = extract_rss_items(root)
-        rss_json = json.dumps(rss_items, indent=4, ensure_ascii=False)
-        print(rss_json)
-        with open('rss_feed.json', 'w', encoding='utf-8') as json_file:
-            json_file.write(rss_json)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    all_rss_items = []
+    for url in RSS_FEED_URLS:
+        try:
+            root = fetch_rss_feed(url, HEADERS)
+            rss_items = extract_rss_items(root)
+            all_rss_items.extend(rss_items)
+        except Exception as e:
+            print(f"An error occurred while processing {url}: {e}")
+
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    output_file = os.path.join(OUTPUT_DIR, f'{timestamp} rss_feed.json')
+    save_to_json(all_rss_items, output_file)
+    update_cumulative_file(all_rss_items, CUMULATIVE_FILE)
 
 
 if __name__ == "__main__":
